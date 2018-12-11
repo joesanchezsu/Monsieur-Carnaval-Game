155a8191e8965ce274a704826e4f93d2a1176caa
Eric Sanchez <jsanchezsua@ensc.fr>
2018-12-11 19:08:45 +0100

    Fusion

---
+++ PlatformGame/Assets/BackgroundController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundController : MonoBehaviour {
+
+	public bool PopUpActive {get; set;}
+	
+	// Use this for initialization
+	void Start () {
+		PopUpActive = false;
+		GetComponent<Image>().color = new Color(1f, 1f, 1f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(PopUpActive){
+			GetComponent<Image>().color = new Color(0.2f, 0.2f, 0.2f, 0.5f);
+		} else {
+			GetComponent<Image>().color = new Color(1f, 1f, 1f, 0f);
+		} 
+	}
+}
